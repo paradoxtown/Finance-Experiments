b69bce9a1b32a2b1bfaaa277a37373e932bb33a4
paradoxtown <parahyacinth@gmail.com>
2023-08-09 22:28:47 +0100

    update strategy

--- simulator.py
+++ simulator.py
@@ -14,8 +14,18 @@ class MyStrategy(Strategy):
             self.buy(size=.2)
         elif forecast == 0 and not self.position.is_short:
             self.sell(size=.2)
-            
+
+class LongOnly(Strategy):
+    def init(self):
+        self.I(lambda x: x, self.data['y_true'], name='y_true')
+        self.I(lambda x: x, self.data['y_pred'], name='y_pred')
+    
+    def next(self):
+        forecast = self.data['y_pred'][-1]
+        
+        if forecast == 1:
+            self.buy(size=.2)
 
 
 def backtest(data):
-    return Backtest(data, MyStrategy, cash=10000, commission=.002)
+    return Backtest(data, LongOnly, cash=10000, commission=.002)
